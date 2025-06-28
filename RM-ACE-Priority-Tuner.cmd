d7c6220b6cc7ae2b23683a96e5d857cb58e0603f
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-06-28 08:41:09 +0800

    Update RM-ACE-Priority-Tuner.cmd

--- RM-ACE-Priority-Tuner.cmd
+++ RM-ACE-Priority-Tuner.cmd
@@ -2,12 +2,17 @@
 chcp 65001 > nul
 cls
 
+echo.
+echo 程序来源:https://github.com/BlazeSnow/ACE-Priority-Tuner
+echo Copyright (c) 2025 BlazeSnow. 保留所有权利.
+echo.
+
 :: 管理员权限检测
 NET SESSION >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     echo.
-    echo 错误:此脚本需要管理员权限才能运行
-    echo 请右键此文件,选择"以管理员身份运行"
+    echo 错误:此脚本需要管理员权限才能运行.
+    echo 请右键此文件,选择"以管理员身份运行".
     echo.
     pause
     exit /b 1
@@ -29,7 +34,7 @@ for %%A in (%APPS%) do (
 )
 
 echo.
-echo 所有注册表处理完毕
+echo 所有注册表处理完毕.
 echo.
 pause
 exit
